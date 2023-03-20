cf9c34372eb786374addfb8d13252079412c43f0
Redrum <typrzemek@gmail.com>
2023-03-20 21:35:04 +0100

    Add new and Delete test

--- tests/chapter one test basics/good_practices.spec.ts
+++ tests/chapter one test basics/good_practices.spec.ts
@@ -1,10 +1,37 @@
 import { test, expect } from '@playwright/test';
 
-test.only('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   const url = 'https://the-internet.herokuapp.com/';
 
   await page.goto(url);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('The Internet');
+  await expect(page).toHaveTitle('The Internet!!');
+});
+
+test('add new element', async ({ page }) => {
+  const url = 'https://the-internet.herokuapp.com/';
+
+  await page.goto(url);
+
+  await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+
+  // Expect a text "Delete" -  use Xpath.
+  await expect(page.locator('//*[@id="elements"]/button')).toHaveText('Delete');
+
+});
+
+test.only('delete element', async ({ page }) => {
+  const url = 'https://the-internet.herokuapp.com/';
+
+  await page.goto(url);
+
+  await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await page.getByRole('button', { name: 'Delete' }).first().click();
+
+  // Expect a return result= 1.
+  await expect(page.getByRole('button', { name: 'Delete' })).toHaveCount(1)
 });
